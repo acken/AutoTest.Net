39545e33497f476a0af8af5cb3af4945c100f1d6
Svein Arne Ackenhausen <acken@acken.no>
2010-06-15 14:35:35 +0200

    Changed to event based feedback from processes

--- src/AutoTest.Core/TestRunners/TestRunners/MSTestRunner.cs
+++ src/AutoTest.Core/TestRunners/TestRunners/MSTestRunner.cs
@@ -10,8 +10,10 @@ using System.IO;
 namespace AutoTest.Core.TestRunners.TestRunners
 {
     class MSTestRunner : ITestRunner
-    {readonly string _unitTestExe;
-        static readonly ILog _logger = LogManager.GetLogger(typeof(NUnitTestRunner));
+    {
+        private readonly string _unitTestExe;
+        private List<TestResult> _testResult = new List<TestResult>();
+        static readonly ILog _logger = LogManager.GetLogger(typeof(MSTestRunner));
 
         public MSTestRunner(IConfiguration configuration)
         {
@@ -23,46 +25,46 @@ namespace AutoTest.Core.TestRunners.TestRunners
         public TestRunResults RunTests(string assemblyName)
         {
             _logger.InfoFormat("Running unit tests using \"{0}\" against assembly {1}.", Path.GetFileName(_unitTestExe), Path.GetFileName(assemblyName));
-            ProcessStartInfo psi = new ProcessStartInfo(_unitTestExe,
+            var proc = new Process();
+            proc.OutputDataReceived += proc_OutputDataReceived;
+            proc.StartInfo = new ProcessStartInfo(_unitTestExe,
                                                         "/testcontainer:\"" + assemblyName + "\"");
-            psi.WorkingDirectory = Path.GetDirectoryName(_unitTestExe);
-            psi.RedirectStandardOutput = true;
-            psi.WindowStyle = ProcessWindowStyle.Hidden;
-            psi.UseShellExecute = false;
-            psi.CreateNoWindow = true;
+            proc.StartInfo.WorkingDirectory = Path.GetDirectoryName(assemblyName);
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.CreateNoWindow = true;
 
-            Process proc = Process.Start(psi);
-
+            proc.Start();
             proc.WaitForExit();
 
-            var results = new List<TestResult>();
-            while (!proc.StandardOutput.EndOfStream)
-            {
-                string line = proc.StandardOutput.ReadLine();
-                string detail = String.Empty;
-
-                // "Tests run: 1, Failures: 1, Not run: 0, Time: 0.116 seconds"
-                if (line.StartsWith("Tests run:"))
-                    Console.WriteLine(line);
-                if (line.Contains("]"))
-                    detail = line.Substring(line.IndexOf("]") + 1).Trim();
-
-                if (line.StartsWith("[pass") || line.StartsWith("[success"))
-                {
-                    results.Add(new TestResult(TestStatus.Passed, detail));
-                }
-                else if (line.StartsWith("[fail"))
-                {
-                    results.Add(new TestResult(TestStatus.Failed, detail));
-                }
-                else if (line.StartsWith("[ignore"))
-                {
-                    results.Add(new TestResult(TestStatus.Ignored, detail));
-                }
-            }
-            return new TestRunResults(results);
+            return new TestRunResults(_testResult);
         }
 
         #endregion
+
+        void proc_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            string line = e.Data;
+            string detail = String.Empty;
+
+            // "Tests run: 1, Failures: 1, Not run: 0, Time: 0.116 seconds"
+            if (line.StartsWith("Tests run:"))
+                Console.WriteLine(line);
+            if (line.Contains("]"))
+                detail = line.Substring(line.IndexOf("]") + 1).Trim();
+
+            if (line.StartsWith("[pass") || line.StartsWith("[success"))
+            {
+                _testResult.Add(new TestResult(TestStatus.Passed, detail));
+            }
+            else if (line.StartsWith("[fail"))
+            {
+                _testResult.Add(new TestResult(TestStatus.Failed, detail));
+            }
+            else if (line.StartsWith("[ignore"))
+            {
+                _testResult.Add(new TestResult(TestStatus.Ignored, detail));
+            }
+        }
     }
 }
--- src/AutoTest.Core/TestRunners/TestRunners/NUnitTestRunner.cs
+++ src/AutoTest.Core/TestRunners/TestRunners/NUnitTestRunner.cs
@@ -11,12 +11,13 @@ namespace AutoTest.Core.TestRunners.TestRunners
 {
     class NUnitTestRunner : ITestRunner
     {
-        readonly string _unitTestExe;
+        private readonly string _unitTestExe;
+        private List<TestResult> _testResult = new List<TestResult>();
         static readonly ILog _logger = LogManager.GetLogger(typeof(NUnitTestRunner));
 
         public NUnitTestRunner(IConfiguration configuration)
         {
-            _unitTestExe = configuration.NunitTestRunner;
+            _unitTestExe = configuration.MSTestRunner;
         }
 
         #region ITestRunner Members
@@ -24,46 +25,46 @@ namespace AutoTest.Core.TestRunners.TestRunners
         public TestRunResults RunTests(string assemblyName)
         {
             _logger.InfoFormat("Running unit tests using \"{0}\" against assembly {1}.", Path.GetFileName(_unitTestExe), Path.GetFileName(assemblyName));
-            ProcessStartInfo psi = new ProcessStartInfo(_unitTestExe,
+            var proc = new Process();
+            proc.OutputDataReceived += proc_OutputDataReceived;
+            proc.StartInfo = new ProcessStartInfo(_unitTestExe,
                                                         "\"" + assemblyName + "\"");
-            psi.WorkingDirectory = Path.GetDirectoryName(_unitTestExe);
-            psi.RedirectStandardOutput = true;
-            psi.WindowStyle = ProcessWindowStyle.Hidden;
-            psi.UseShellExecute = false;
-            psi.CreateNoWindow = true;
+            proc.StartInfo.WorkingDirectory = Path.GetDirectoryName(assemblyName);
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.CreateNoWindow = true;
 
-            Process proc = Process.Start(psi);
-
+            proc.Start();
             proc.WaitForExit();
 
-            var results = new List<TestResult>();
-            while (!proc.StandardOutput.EndOfStream)
-            {
-                string line = proc.StandardOutput.ReadLine();
-                string detail = String.Empty;
-
-                // "Tests run: 1, Failures: 1, Not run: 0, Time: 0.116 seconds"
-                if (line.StartsWith("Tests run:"))
-                    Console.WriteLine(line);
-                if (line.Contains("]"))
-                    detail = line.Substring(line.IndexOf("]") + 1).Trim();
-
-                if (line.StartsWith("[pass") || line.StartsWith("[success"))
-                {
-                    results.Add(new TestResult(TestStatus.Passed, detail));
-                }
-                else if (line.StartsWith("[fail"))
-                {
-                    results.Add(new TestResult(TestStatus.Failed, detail));
-                }
-                else if (line.StartsWith("[ignore"))
-                {
-                    results.Add(new TestResult(TestStatus.Ignored, detail));
-                }
-            }
-            return new TestRunResults(results);
+            return new TestRunResults(_testResult);
         }
 
         #endregion
+
+        void proc_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            string line = e.Data;
+            string detail = String.Empty;
+
+            // "Tests run: 1, Failures: 1, Not run: 0, Time: 0.116 seconds"
+            if (line.StartsWith("Tests run:"))
+                Console.WriteLine(line);
+            if (line.Contains("]"))
+                detail = line.Substring(line.IndexOf("]") + 1).Trim();
+
+            if (line.StartsWith("[pass") || line.StartsWith("[success"))
+            {
+                _testResult.Add(new TestResult(TestStatus.Passed, detail));
+            }
+            else if (line.StartsWith("[fail"))
+            {
+                _testResult.Add(new TestResult(TestStatus.Failed, detail));
+            }
+            else if (line.StartsWith("[ignore"))
+            {
+                _testResult.Add(new TestResult(TestStatus.Ignored, detail));
+            }
+        }
     }
 }
