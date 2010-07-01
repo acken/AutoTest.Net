6b846141b8daa2ae8164df24fe0609d6410e3e78
Svein Arne Ackenhausen <acken@acken.no>
2010-07-01 08:38:52 +0200

    Added some project change consumer tests. Need to finish up service locator stuff to finish up these tests

--- src/AutoTest.Test/Core/Messaging/MessageConsumers/ProjectChangeConsumerTest.cs
+++ src/AutoTest.Test/Core/Messaging/MessageConsumers/ProjectChangeConsumerTest.cs
@@ -5,17 +5,41 @@ using System.Text;
 using NUnit.Framework;
 using AutoTest.Core.Messaging.MessageConsumers;
 using AutoTest.Core.Messaging;
+using Rhino.Mocks;
 
 namespace AutoTest.Test.Core.Messaging.MessageConsumers
 {
     [TestFixture]
     public class ProjectChangeConsumerTest
     {
+        private ProjectChangeConsumer _consumer;
+        private IMessageBus _bus;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _bus = MockRepository.GenerateMock<IMessageBus>();
+            _consumer = new ProjectChangeConsumer(_bus, null, null);
+        }
+
         [Test]
         public void Should_be_a_blocking_consumer()
         {
-            var consumer = new ProjectChangeConsumer(null, null, null);
-            consumer.ShouldBeOfType<IBlockingConsumerOf<ProjectChangeMessage>>();
+            _consumer.ShouldBeOfType<IBlockingConsumerOf<ProjectChangeMessage>>();
+        }
+
+        [Test]
+        public void Should_publish_run_started_message()
+        {
+            _consumer.Consume(new ProjectChangeMessage());
+            _bus.AssertWasCalled(b => b.Publish<RunStartedMessage>(null), b => b.IgnoreArguments());
+        }
+
+        [Test]
+        public void Should_publish_run_finished_message()
+        {
+            _consumer.Consume(new ProjectChangeMessage());
+            _bus.AssertWasCalled(b => b.Publish<RunFinishedMessage>(null), b => b.IgnoreArguments());
         }
     }
 }
