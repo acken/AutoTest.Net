915fcd7172f64cafc46223438b3378c97e581871
acken <acken@acken.no>
2010-08-08 14:17:49 +0200

    Added information message about debug logging

--- src/AutoTest.WinForms/Program.cs
+++ src/AutoTest.WinForms/Program.cs
@@ -6,6 +6,7 @@ using AutoTest.Core.Configuration;
 using System.Reflection;
 using Castle.MicroKernel.Registration;
 using System.IO;
+using AutoTest.Core.Messaging;
 
 namespace AutoTest.WinForms
 {
@@ -26,6 +27,7 @@ namespace AutoTest.WinForms
 			{
 			ConfigureApplication();
         	var overviewForm = BootStrapper.Services.Locate<IOverviewForm>();
+			notifyOnLoggingSetup();
         	Application.Run(overviewForm.Form);
         	BootStrapper.ShutDown();
 			}
@@ -71,5 +73,11 @@ namespace AutoTest.WinForms
                 .Register(Component.For<IOverviewForm>().ImplementedBy<FeedbackForm>())
                 .Register(Component.For<IInformationForm>().ImplementedBy<InformationForm>());
         }
+		
+		private static void notifyOnLoggingSetup()
+		{
+			var bus = BootStrapper.Services.Locate<IMessageBus>();
+			bus.Publish<InformationMessage>(new InformationMessage("Debugging enabled"));
+		}
     }
 }
