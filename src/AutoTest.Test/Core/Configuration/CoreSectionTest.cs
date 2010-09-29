06b3f14ec26ef0eb2b945e7ed4e8f56971677096
acken <acken@acken.no>
2010-09-29 17:40:41 +0200

    Started on core section tests

---
+++ src/AutoTest.Test/Core/Configuration/CoreSectionTest.cs
@@ -0,0 +1,17 @@
+using System;
+using NUnit.Framework;
+using AutoTest.Core.Configuration;
+namespace AutoTest.Test.Core.Configuration
+{
+	[TestFixture]
+	public class CoreSectionTest
+	{
+		[Test]
+		public void Should_read_merge_from_node()
+		{
+			var section = new CoreSection();
+			Assert.Ignore("Not implemented yet");
+		}
+	}
+}
+
