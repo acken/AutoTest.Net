f5bd5b1cbc2ce3753a3d123c2fb2b2fdf1c4486d
acken <acken@acken.no>
2010-10-13 12:40:38 +0200

    Some test tweaking

--- src/AutoTest.Test/Core/FileSystem/DirectoryWatcherTests.cs
+++ src/AutoTest.Test/Core/FileSystem/DirectoryWatcherTests.cs
@@ -72,7 +72,7 @@ namespace AutoTest.Test.Core
             // Write twice
             File.WriteAllText(file, "meh ");
             using (var writer = new StreamWriter(file, true)) { writer.WriteLine("some text"); }
-            Thread.Sleep(250);
+            Thread.Sleep(350);
             
             messageBus.AssertWasCalled(
                 m => m.Publish<FileChangeMessage>(
