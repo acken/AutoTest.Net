24e454c8bdfef2fda42fa0859314c8638f42923c
acken <acken@acken.no>
2011-01-09 14:15:07 +0100

    Rearanged projects

---
+++ src/AutoTest.Runners.NUnit/NUnitOptionsParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AutoTest.TestRunners.Shared;
+
+namespace AutoTest.TestRunners.NUnit
+{
+    class NUnitOptionsParser
+    {
+        private RunnerOptions _options;
+
+        public string Tests { get; private set; }
+        public string Categories { get; private set; }
+        public string Assemblies { get; private set; }
+        public string Framework { get; private set; }
+
+        public NUnitOptionsParser(RunnerOptions options)
+        {
+            _options = options;
+        }
+
+        public void Parse()
+        {
+            Assemblies = @"C:\Users\ack\src\AutoTest.Net\src\AutoTest.Test\bin\AutoTest.NET\AutoTest.Test.dll";
+            Tests = "AutoTest.Test.Core.TestRunners.When_parsing_a_stack_line_with_description.Should_not_parse_the_file_name";
+        }
+    }
+}
---
+++ src/AutoTest.TestRunners/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutoTest.TestRunners
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var options = new RunOptions();
+            //options.AddRunner(new RunnerOptions());
+            //var runner = new NUnitProxy.NUnitRunner();
+            //runner.Initialize();
+            //runner.Execute(options.Runners.First());
+        }
+    }
+}
