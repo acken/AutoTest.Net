88babfd2d0da83857da3f3aad81a6d597a534341
acken <acken@acken.no>
2011-01-09 11:13:36 +0100

    hacked out a working nunit runner

--- src/AutoTest.TestRunner/Program.cs
+++ src/AutoTest.TestRunner/Program.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace AutoTest.TestRunner
+namespace AutoTest.TestRunners
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var options = new RunOptions();
+            options.AddRunner(new RunnerOptions());
+            var runner = new NUnitProxy.NUnitRunner();
+            runner.Initialize();
+            runner.Execute(options.Runners.First());
         }
     }
 }
