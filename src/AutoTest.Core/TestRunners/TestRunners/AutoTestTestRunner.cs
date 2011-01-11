c0f381744fa6f455d35263d788fca031b787f770
acken <acken@acken.no>
2011-01-11 23:58:09 +0100

    Started on autotest runner option generator

--- src/AutoTest.Core/TestRunners/TestRunners/AutoTestTestRunner.cs
+++ src/AutoTest.Core/TestRunners/TestRunners/AutoTestTestRunner.cs
@@ -7,6 +7,9 @@ using AutoTest.Messages;
 using AutoTest.Core.Messaging.MessageConsumers;
 using AutoTest.Core.FileSystem;
 using AutoTest.Core.Configuration;
+using AutoTest.TestRunners.Shared;
+using AutoTest.TestRunners.Shared.Plugins;
+using System.IO;
 
 namespace AutoTest.Core.TestRunners.TestRunners
 {
@@ -28,8 +31,7 @@ namespace AutoTest.Core.TestRunners.TestRunners
 
         public bool CanHandleTestFor(string assembly)
         {
-            var references = _referenceResolver.GetReferences(assembly);
-            return references.Contains("nunit.framework");
+            return assemblyReferences(assembly, "nunit.framework");
         }
 
         public TestRunResults[] RunTests(TestRunInfo[] runInfos)
@@ -37,7 +39,60 @@ namespace AutoTest.Core.TestRunners.TestRunners
             if (!_configuration.UseAutoTestTestRunner)
                 return new TestRunResults[] { };
 
+            var optionsFile = generateOptions(runInfos);
+
             return new TestRunResults[] { };
+        }
+
+        private string generateOptions(TestRunInfo[] runInfos)
+        {
+            var plugins = new List<Plugin>();
+            var options = new RunOptions();
+            var nunitInfos = runInfos.Where(x => canHandleNUnit(x));
+            if (nunitInfos.Count() > 0)
+            {
+                plugins.Add(new Plugin(Path.GetFullPath(Path.Combine("TestRunners", "AutoTest.TestRunners.NUnit.dll")), "AutoTest.TestRunners.NUnit.Runner"));
+                var runner = getRunnerOptions(nunitInfos, "NUnit", TestRunner.NUnit, getFramework);
+                options.AddTestRun(runner);
+            }
+            return "";
+        }
+
+        private RunnerOptions getRunnerOptions(IEnumerable<TestRunInfo> nunitInfos, string id, TestRunner testRunner, Func<TestRunInfo, string> frameworkEvaluator)
+        {
+            var runner = new RunnerOptions(id);
+            foreach (var info in nunitInfos)
+            {
+                var assembly = new AssemblyOptions(info.Assembly, frameworkEvaluator.Invoke(info));
+                assembly.AddTests(info.GetTestsFor(testRunner));
+                assembly.AddTests(info.GetTestsFor(TestRunner.Any));
+            }
+            return runner;
+        }
+
+        private string getFramework(TestRunInfo info)
+        {
+            if (Environment.OSVersion.Platform.Equals(PlatformID.Unix) || Environment.OSVersion.Platform.Equals(PlatformID.MacOSX))
+                return null;
+
+            if (info.Project == null || info.Project.Value == null)
+                return null;
+            if (info.Project.Value.Framework == null)
+                return null;
+            return info.Project.Value.Framework.Replace("v", "");
+        }
+
+        private bool canHandleNUnit(TestRunInfo info)
+        {
+            if (info.Project != null && info.Project.Value != null)
+                return info.Project.Value.ContainsNUnitTests;
+            return assemblyReferences(info.Assembly, "nunit.framework");
+        }
+
+        private bool assemblyReferences(string assembly, string reference)
+        {
+            var references = _referenceResolver.GetReferences(assembly);
+            return references.Contains(reference);
         }
     }
 }
