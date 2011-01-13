c766392b2eb897427bd4fb3b36f67f73d05fa10e
acken <acken@acken.no>
2011-01-13 03:30:46 +0100

    Cosmetic stuff

--- src/AutoTest.TestRunner/Plugins/AutoTest.TestRunners.NUnit.Tests/RunnerTests.cs
+++ src/AutoTest.TestRunner/Plugins/AutoTest.TestRunners.NUnit.Tests/RunnerTests.cs
@@ -28,7 +28,7 @@ namespace AutoTest.TestRunners.NUnit.Tests
             Assert.That(results.ElementAt(0).State, Is.EqualTo(Shared.Results.TestState.Failed));
             Assert.That(results.ElementAt(0).TestName, Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1.Should_fail"));
             Assert.That(results.ElementAt(0).StackLines.Count(), Is.EqualTo(1));
-            Assert.That(File.Exists(results.ElementAt(0).StackLines.ElementAt(0).File), Is.True);
+            Assert.IsTrue(File.Exists(results.ElementAt(0).StackLines.ElementAt(0).File), "Did not find " + results.ElementAt(0).StackLines.ElementAt(0).File);
             Assert.That(results.ElementAt(0).StackLines.ElementAt(0).Method.Replace(" ()", "()"), Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1.Should_fail()"));
             Assert.That(results.ElementAt(0).StackLines.ElementAt(0).Line, Is.EqualTo(23));
 
@@ -37,7 +37,7 @@ namespace AutoTest.TestRunners.NUnit.Tests
             Assert.That(results.ElementAt(1).State, Is.EqualTo(Shared.Results.TestState.Ignored));
             Assert.That(results.ElementAt(1).TestName, Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1.Should_ignore"));
             Assert.That(results.ElementAt(1).StackLines.Count(), Is.EqualTo(1));
-            Assert.That(File.Exists(results.ElementAt(0).StackLines.ElementAt(0).File), Is.True);
+            Assert.IsTrue(File.Exists(results.ElementAt(0).StackLines.ElementAt(0).File), "Did not find " + results.ElementAt(0).StackLines.ElementAt(0).File);
             Assert.That(results.ElementAt(1).StackLines.ElementAt(0).Method.Replace(" ()", "()"), Is.EqualTo("AutoTest.TestRunners.NUnit.Tests.TestResource.Fixture1.Should_ignore()"));
             Assert.That(results.ElementAt(1).StackLines.ElementAt(0).Line, Is.EqualTo(30));
 
