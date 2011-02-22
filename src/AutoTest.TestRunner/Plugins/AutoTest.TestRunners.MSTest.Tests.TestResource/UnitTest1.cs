7d6edd469419ae71269f359deb1a0b4d5d07dc82
acken <acken@acken.no>
2011-02-22 15:25:13 +0100

    Added some stuff to mstest runner for autotest.testrunner

---
+++ src/AutoTest.TestRunner/Plugins/AutoTest.TestRunners.MSTest.Tests.TestResource/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoTest.TestRunners.MSTest.Tests.TestResource
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
