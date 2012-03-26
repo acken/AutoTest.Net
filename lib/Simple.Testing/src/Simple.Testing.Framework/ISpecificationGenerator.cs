3aebccf0543fd316f7f0cae3f45018b66095545b
Greg Young <gregoryyoung1@gmail.com>
2012-03-26 10:19:40 -0400

    remove Simple.Testing source (only leave output file which is what things link to)

--- lib/Simple.Testing/src/Simple.Testing.Framework/ISpecificationGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Simple.Testing.Framework
-{
-    public interface ISpecificationGenerator
-    {
-        IEnumerable<SpecificationToRun> GetSpecifications();
-    }
-}
