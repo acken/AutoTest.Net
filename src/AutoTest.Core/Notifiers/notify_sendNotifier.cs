5c367ca3978b8817d256d3a9fc535483bce8b6b0
ack <ack@ack-slim.(none)>
2013-02-09 23:43:10 +0100

    Removed critical as it in gnome shell makes the user click an x to kill the notification

--- src/AutoTest.Core/Notifiers/notify_sendNotifier.cs
+++ src/AutoTest.Core/Notifiers/notify_sendNotifier.cs
@@ -50,7 +50,7 @@ namespace AutoTest.Core.Notifiers
 					icon += "/Icons/circleFAIL.png";
 					break;
 			}
-			string args = "\"AutoTest.Net\" \"" + msg + "\" --icon=\"" + icon + "\" --urgency=critical";
+			string args = "\"AutoTest.Net\" \"" + msg + "\" --icon=\"" + icon + "\"";
 			var process = new Process();
             process.StartInfo = new ProcessStartInfo("notify-send", args);
 			process.StartInfo.CreateNoWindow = true;
