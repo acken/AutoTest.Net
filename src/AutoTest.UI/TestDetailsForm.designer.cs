67d3e364470f1f742f46d79c72ac7f7729a1514b
acken <acken@acken.no>
2013-06-05 19:05:11 +0200

    Added manual resizing of detail form on nix systems

--- src/AutoTest.UI/TestDetailsForm.designer.cs
+++ src/AutoTest.UI/TestDetailsForm.designer.cs
@@ -96,7 +96,6 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(544, 208);
             this.Controls.Add(this.textBoxFocusHolder);
             this.Controls.Add(this.pictureBoxLine);
