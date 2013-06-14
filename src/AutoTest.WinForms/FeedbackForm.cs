dbc8c4a37b47f63bb06de509c0b4b1966bb10ecf
acken <acken@acken.no>
2013-06-14 15:46:14 +0200

    Moved the initial text to user control

--- src/AutoTest.WinForms/FeedbackForm.cs
+++ src/AutoTest.WinForms/FeedbackForm.cs
@@ -41,7 +41,6 @@ namespace AutoTest.WinForms
                 new LabelItembehaviour(runFeedback.linkLabelTestDetails),
                 new LabelItembehaviour(runFeedback.linkLabelErrorDescription));
             runFeedback.SetFeedbackProvider(_provider);
-            _provider.PrintMessage(new UI.RunMessages(UI.RunMessageType.Normal, "Collecting source and project information..."));
         }
 
         private void addContextMenues()
